98e019ab6f89b9618c7db1a663ea1791442a07d2
mpalmeos <maie.palmeos@itcollege.ee>
2017-02-24 16:42:22 +0200

    pealehe täiendus

--- index.php
+++ index.php
@@ -9,9 +9,19 @@
 	<body>
 		<h1>Welcome to my homepage!</h1>
 		<img src="Kass.jpg" alt="Sorry, missed it" width="300" height="400"/> <br/>
+		
 		<div>
 		<a href="/~mpalmeos/Harjutus1">Praktikum 1</a> <br/>
 		<a href="/~mpalmeos/Harjutus2">Praktikum 2</a>
+		</div>
+		
+		<div>
+		<iframe src="/~mpalmeos/Harjutus2/praktikum2.html" name="iraam" height="500" width="500">
+		</iframe> <br/><br/>
+		<a href="Harjutus2/harjutus1.html" target="iraam">Harjutus 1</a> <br/><br/>
+		<a href="Harjutus2/harjutus2.html" target="iraam">Harjutus 2</a><br/><br/>
+		<a href="Harjutus2/harjutus2-2.html" target="iraam">Harjutus 2, osa 2</a><br/><br/>
+		<a href="Harjutus2/praktikum2.html" target="iraam">Galerii ülesanne</a><br/>
 		</div>
 		
 	<?php 
