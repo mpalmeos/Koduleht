72a81ef0959f6f024569437cf4587a91383a9341
mpalmeos <maie.palmeos@itcollege.ee>
2017-03-01 19:14:38 +0200

    Adding validators

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	</head>
 	
 	<body>
-		<div style="background: lightblue; text-align: justify;">
+		<div style="background: lightblue; text-align: justify;" id="pilt">
 		<div class="must">
 		<h1 id="Pealkiri">Welcome to my homepage!</h1>
 		<img src="Kass.jpg" alt="Sorry, missed it" width="300" height="400"/> <br/>
@@ -26,10 +26,12 @@
 		<br/>
 		<iframe src="/~mpalmeos/Harjutus2/praktikum2.html" name="iraam" height="500" width="500">
 		</iframe> <br/><br/>
-		<a href="Harjutus2/harjutus1.html" target="iraam">Harjutus 1</a> <br/>
-		<a href="Harjutus2/harjutus2.html" target="iraam">Harjutus 2</a><br/>
-		<a href="Harjutus2/harjutus2-2.html" target="iraam">Harjutus 2, osa 2</a><br/>
-		<a href="Harjutus2/praktikum2.html" target="iraam">Galerii ülesanne</a><br/>
+		<ul id="loend">
+		<li><a href="Harjutus2/harjutus1.html" target="iraam">Harjutus 1</a> </li>
+		<li><a href="Harjutus2/harjutus2.html" target="iraam">Harjutus 2</a></li>
+		<li><a href="Harjutus2/harjutus2-2.html" target="iraam">Harjutus 2, osa 2</a></li>
+		<li><a href="Harjutus2/praktikum2.html" target="iraam">Galerii ülesanne</a></li>
+		</ul>
 		</div>
 		
 	<?php 
@@ -45,5 +47,13 @@
     			
    			 mysqli_close($l);			
 			?>
+	<div>
+		<a href="http://validator.w3.org/check?uri=referer">
+		<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+		</a> <br/>
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">
+		<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+		</a>
+	</div>
 	</body>
 </html>
