3842855d64e5709a9da7ab5dc23bc89320393969
mpalmeos <maie.palmeos@itcollege.ee>
2017-03-12 10:37:48 +0200

    ringid

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 		<a href="http://validator.w3.org/check?uri=referer">
 		<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
 		</a> <br/>
-		<a href="http://jigsaw.w3.org/css-validator/check/referer">
+		<a href="http://jigsaw.w3.org/css-validator/check/referer" id="CSSvalid">
 		<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
 		</a>
 	</div>
