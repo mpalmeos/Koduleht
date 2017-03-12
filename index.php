63311011e21513f4e8beae02725a231663a8a39a
mpalmeos <maie.palmeos@itcollege.ee>
2017-03-12 10:52:56 +0200

    more fixes

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 		<a href="http://validator.w3.org/check?uri=referer">
 		<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
 		</a> <br/>
-		<a href="http://jigsaw.w3.org/css-validator/check/referer" id="CSSvalid">
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">
 		<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
 		</a>
 	</div>
