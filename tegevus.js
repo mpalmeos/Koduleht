c05fb8ddcedcfbb1d717f15c253582f3bf7a9ca3
mpalmeos <maie.palmeos@itcollege.ee>
2017-03-18 15:01:06 +0200

    abakuse ülesanne

--- tegevus.js
+++ tegevus.js
@@ -1,15 +1,13 @@
 window.onload = function() {
-	var b = document.getElementById('parl');
-	var massiiv = [];
-  massiiv.push(b);
-  b.onclick = function() {
-    for (i = 0; i < massiiv.length; i++) {
-      var c = massiiv[i];
-      if (c.style.cssFloat == "left") {
-        b.style.cssFloat = "right";
-      } else if (c.style.cssFloat == "right") {
-        b.style.cssFloat = "left";
-      }
-    }
-  }
-}
+	var b = document.getElementsByClassName("parl");
+	
+    for (var i = 0; i < b.length; i++) {
+		b[i].onclick = function() {
+			if (window.getComputedStyle(b).getPropertyValue("float") == "left"){
+				b.style.cssFloat = "right";
+			} else {
+				b.style.cssFloat = "left";
+			}
+		}
+	}
+ }
