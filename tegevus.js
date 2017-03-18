aa8034528cd71e1cc528c2e1e26d24831ebe654c
mpalmeos <maie.palmeos@itcollege.ee>
2017-03-18 13:45:03 +0200

    JS jama

--- tegevus.js
+++ tegevus.js
@@ -1,14 +1,15 @@
 window.onload = function() {
-	var b = document.getElementById('parl')
-	var massiiv = new Array(b);
-	massiiv.onclick = function(){
-		for (i=0; i<massiiv.length; i++){
-			var c = massiiv[i];			
-			if (c.style.cssFloat == left){
-				c.style.cssFloat = right;
-			} else if (c.style.cssFloat == right) {
-				c.style.cssFloat = left;
-			}
-		}
-	}
+	var b = document.getElementById('parl');
+	var massiiv = [];
+  massiiv.push(b);
+  b.onclick = function() {
+    for (i = 0; i < massiiv.length; i++) {
+      var c = massiiv[i];
+      if (c.style.cssFloat == "left") {
+        b.style.cssFloat = "right";
+      } else if (c.style.cssFloat == "right") {
+        b.style.cssFloat = "left";
+      }
+    }
+  }
 }
