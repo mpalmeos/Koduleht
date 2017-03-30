4605c6dba550fb29e6e2a87458a689548e6a3945
Maie Palmeos <maie.palmeos@itcollege.ee>
2017-03-30 20:50:20 +0300

    Syntax error

--- PHP/phpyl3.php
+++ PHP/phpyl3.php
@@ -8,9 +8,9 @@
 <?php
 
 $helmed=array(
-    array("nimi"=>"punane ämblik", "pilt"=>"http://gdurl.com/vruC", "asi"="pross", "kandes"=>"hirmus"),
-	array("nimi"=>"orhidee", "pilt"=>"http://gdurl.com/nfxU", "asi"="pross", "kandes"=>"ametlik"),
-	array("nimi"=>"draakon", "pilt"=>"http://gdurl.com/mnaq", "asi"="kujuke", "kandes"=>", et kukub maha"),
+    array("nimi" => "punane ämblik", "pilt" => "http://gdurl.com/vruC", "asi" => "pross", "kandes" => "hirmus"),
+	array("nimi" => "orhidee", "pilt" => "http://gdurl.com/nfxU", "asi" => "pross", "kandes" => "ametlik"),
+	array("nimi" => "draakon", "pilt" => "http://gdurl.com/mnaq", "asi" => "kujuke", "kandes" => ", et kukub maha"),
 );
 
 foreach($helmed as $ehe){
