849542fe7a72f175e887cd20a718f69ad1cc6616
Maie Palmeos <maie.palmeos@itcollege.ee>
2017-04-23 20:34:27 +0300

    viimane katse

--- Yl10/kontrolleriga/kontroller.php
+++ Yl10/kontrolleriga/kontroller.php
@@ -1,28 +1,24 @@
 <?php
 session_start();
-
 require_once("head.html");
-
 $pildid = array(
-    1=>array('src'=>"pildid/nameless1.jpg", 'alt'=>"nimetu 1"),
-	2=>array('src'=>"pildid/nameless2.jpg", 'alt'=>"nimetu 2"),
-	3=>array('src'=>"pildid/nameless3.jpg", 'alt'=>"nimetu 3"),
-	4=>array('src'=>"pildid/nameless4.jpg", 'alt'=>"nimetu 4"),
-	5=>array('src'=>"pildid/nameless5.jpg", 'alt'=>"nimetu 5"),
-	6=>array('src'=>"pildid/nameless6.jpg", 'alt'=>"nimetu 6"),
+		1=>array('src'=>"pildid/nameless1.jpg", 'alt'=>"nimetu 1"),
+		2=>array('src'=>"pildid/nameless2.jpg", 'alt'=>"nimetu 2"),
+		3=>array('src'=>"pildid/nameless3.jpg", 'alt'=>"nimetu 3"),
+		4=>array('src'=>"pildid/nameless4.jpg", 'alt'=>"nimetu 4"),
+		5=>array('src'=>"pildid/nameless5.jpg", 'alt'=>"nimetu 5"),
+		6=>array('src'=>"pildid/nameless6.jpg", 'alt'=>"nimetu 6"),
 	);
-
 $page="pealeht";
 if (isset($_GET['page']) && $_GET['page']!=""){
-	$page = htmlspecialchars($_GET['page']);
+	$page=htmlspecialchars($_GET['page']);
 }
-
 switch($page){
 	case "galerii":
 		include("galerii.php");
 	break;
     case "vote":
-        if(!isset($_SESSION['vote_for'])){
+        if(!isset($_SESSION['voted_for'])){
             include ("vote.php");
         } else {
             include ("tulemus.html");
@@ -42,8 +38,7 @@ switch($page){
 		include("tulemus.html");
 	break;
 	default:
-	include('pealeht.php');
+	 include('pealeht.php');
 }
-
 require_once("footer.html");
 ?>
