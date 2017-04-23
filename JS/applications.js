70e27eb2def2dfc6fd59e463b97c78a999067845
Maie Palmeos <maie.palmeos@itcollege.ee>
2017-04-23 10:43:18 +0300

    Total restart

--- JS/applications.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#marklaud").click(function(){
-		$.ajax({url: "../HTML/bullseye.html", success: function(result){
-			$("$content").html(result);
-		}});
-	});
-});
--- index.php
+++ index.php
@@ -1,46 +1,9 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-		<script src="applications.js"></script>
-		<link rel="stylesheet" type="text/css" href="CSS/Style.css"/>
-		<title>Koduleht</title>
-	</head>
-	<body>
-	
-	<div id="title_banner"> 
-	    <img id="benner" src="http://gdurl.com/7Itq" alt="bites" width="1000" height="100">
-	    <div id="title_text">BEAD BYTE SHOP</div>
-	</div>
-	
-	<div>
-	<ul id="menu">
-		<li><a id="marklaud" href="HTML/bullseye.html">Märklaua harjutus</a> </li>
-		<li><a href="HTML/abakus.html">Abakuse harjutus</a></li>
-		<li><a href="HTML/jquery.html">jQuery harjutus</a></li>
-		<li><a href="PHP/phpyl1.php">PHP ülesanne 1</a></li>
-		<li><a href="PHP/phpyl2.php">PHP ülesanne 2</a></li>
-		<li><a href="PHP/phpyl3.php">PHP ülesanne 3</a></li>
-		<li><a href="PHP/too8-1.php">Tekstitöötluse ülesanne</a></li>
-		<li><a href="PHP/too8-2.php">Serveri kaustatade ülesanne</a></li>
-	</ul>
-	</div>	
-	
-	<div id="content"></div>
-	
-	<div id="HTML">
-		<a href="http://validator.w3.org/check?uri=referer">
-		<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
-		</a>
-	</div>
-	<div id="CSS">
-		<a href="http://jigsaw.w3.org/css-validator/check/referer">
-		<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" height="31" width="88" />
-		</a>
-	</div>
-		
-	<?php 
+<?php require_once('View/pealdis.html');?>
+<?php require_once('View/valik_menuu.html');?>
+
+<div id="content"></div>		
+
+<?php 
 		echo phpversion(); 
 		$host = "localhost";
 		$user = "test";
@@ -51,6 +14,6 @@
 		mysqli_query($l, "SET CHARACTER SET UTF8") or
         	die("Error, ei saa andmebaasi charsetti seatud");
     	mysqli_close($l);			
-	?>
-	</body>
-</html>
+?>
+
+<?php require_once('View/jalats.html');?>
