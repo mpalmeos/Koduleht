f1682adb541ddac3c3bcbd7cb288c43ab3f7d19a
Maie Palmeos <maie.palmeos@itcollege.ee>
2017-05-24 19:33:14 +0300

    galerii fixes

--- Model/picture_show.js
+++ Model/picture_show.js
@@ -1,7 +1,4 @@
-// Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -11,10 +8,8 @@ img.onclick = function(){
     captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 } 
