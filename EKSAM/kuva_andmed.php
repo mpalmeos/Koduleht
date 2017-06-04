e67263299ea98123f222adaf0a133bbfc2578212
mpalmeos <maie.palmeos@itcollege.ee>
2017-06-04 11:55:46 +0300

    kirjutusõigused

--- EKSAM/kuva_andmed.php
+++ EKSAM/kuva_andmed.php
@@ -7,12 +7,12 @@ $kylastajate_arv = "kylastajate_arv.txt";
 
 if(!file_exists($kylastajate_arv)){ //Lahendusel kasutatud: https://hibbard.eu/how-to-make-a-simple-visitor-counter-using-php/
 	$f=fopen($kylastajate_arv, "w");
-	fwrite($f, "0");
+	fwrite($f,"0");
 	fclose($f);
 }
 
 $f=fopen($kylastajate_arv, "r");
-$lugeja=fread($f, filesize($kylastajate_arv));
+$lugeja=fread($f);
 fclose($f);
 
 if(!isset($_SESSION['kylastusi'])){
